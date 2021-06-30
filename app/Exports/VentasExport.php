b0d016c93e13dc3631dd27d974d2b9f4bdeed03d
Alexis Muñoz <45567085+Trxshy@users.noreply.github.com>
2021-06-29 23:47:29 -0400

    Update exportación de documento
    Ahora muestra la descripción en vez de las ID's

--- app/Exports/VentasExport.php
+++ app/Exports/VentasExport.php
@@ -4,6 +4,7 @@ namespace App\Exports;
 
 use App\Models\documento;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Illuminate\Support\Facades\DB;
 
 class VentasExport implements FromCollection
 {
@@ -12,6 +13,13 @@ class VentasExport implements FromCollection
     */
     public function collection()
     {
-        return documento::all();
+        $data = DB::table('documento')
+                ->select('idDocumento','Numero_Documento','Valor_Total','Fecha_emision','Fecha_vencimiento','iva','giro','DescripcionM','Descripcion','DescripcionE')
+                ->join('tipo_movimiento','documento.Tipo_Movimiento_idTipo_Movimiento','=','tipo_movimiento.idTipo_Movimiento')
+                ->join('tipo_documento','documento.Tipo_Documento_idTipo_Documento','=','tipo_documento.idTipo_Documento')
+                ->join('estado','documento.Estado_Venta_idEstado_Venta','=','estado.idEstado')
+                ->get();
+
+        return $data;
     }
 }
