39de68289be15cc1e111fd9634c8bd3b7848827e
th3fxcker <luchitoxpatrick13@gmail.com>
2021-07-15 19:50:59 -0400

    arreglo de export estadistica

--- app/Exports/EstadisticaExport.php
+++ app/Exports/EstadisticaExport.php
@@ -13,24 +13,11 @@ class EstadisticaExport implements FromCollection, WithHeadings
     * @return \Illuminate\Support\Collection
     */
 
-    ##public function collection1(){
-##
-    ##    $Proveedor = DB::table('persona')
-    ##    ->Select( DB::raw("count(Nombre) as Cantidad_Proveedor"))
-    ##    ->get();
-##
-    ##    return $Proveedor;
-    ##}
-##
     public function collection()
     {
         $Cantidad = DB::table('detalle_documento') 
-        ->Select( DB::raw('count(idDetalle_documento)'))
-        ->addSelect( DB::raw('sum(Cantidad)'))
-        ->addSelect( DB::raw('sum(detalle_documento.Valor_Total_Bruto)'))
-        ->addSelect( DB::raw('count(Nombre)'))
-
-        ->join('documento','detalle_documento.Documento_idDocumento','=','documento.idDocumento')
+        ->Select( DB::raw('count(idDetalle_documento)','sum(Cantidad) as ','sum(detalle_documento)','count(Nombre)'))
+        ->join('documento','detalle_documento.Documentos_idDocumentos','=','documento.idDocumento')
         ->join('usuario','documento.Usuario_idUsuario','=','usuario.idUsuario')
         ->join('persona','usuario.Persona_idPersona','=','persona.idPersona')     
         ->get();
